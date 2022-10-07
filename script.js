754996dfa3cf16d289a3add4c6b00e03031e15d2
Diaa-E <blarb21@yahoo.com>
2022-10-07 09:16:07 +0200

    Add board module and reset function

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 "use strict"
+
+const gameBoard = (() => {
+
+    let boardArray;
+
+    const _resetBoard = () => {
+        
+        for (let i = 0; i < 3; i++)
+        {
+            for (let j = 0; j < 3; j++)
+            {
+                boardArray[i][j] = "";
+            }
+        }
+    };
+
+    
+
+})();
