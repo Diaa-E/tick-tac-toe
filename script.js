198d5abf568de464125a4b1149c7767296093df2
Diaa-E <blarb21@yahoo.com>
2022-10-07 09:23:37 +0200

    Fix board initialization error

--- script.js
+++ script.js
@@ -2,19 +2,20 @@
 
 const gameBoard = (() => {
 
-    let boardArray;
+    const emptySlot = " ";
+    let boardArray= [[], [], []];
 
-    const _resetBoard = () => {
+    const resetBoard = () => {
         
         for (let i = 0; i < 3; i++)
         {
             for (let j = 0; j < 3; j++)
             {
-                boardArray[i][j] = "";
-            }
-        }
+                boardArray[i][j] = emptySlot;
+            };
+        };
     };
 
-    
+    return {};
 
 })();
